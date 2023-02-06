46446d56a40d5c6ea3295effa576a7c8fa9498a5
bjornandrip <bjornandrip@gmail.com>
2023-02-06 15:23:30 +0000

    added history guessr to projects

--- src/constants/projectsData.js
+++ src/constants/projectsData.js
@@ -1,5 +1,6 @@
 import beinIMG from '../resources/BeinheilsaBg.png'
 import samasemIMG from '../resources/SamasemBG.png'
+import HistoryGuessrBG from '../resources/HistoryGuessrBG.png'
 export const projectsData = [
     {
         id:1,
@@ -14,5 +15,12 @@ export const projectsData = [
         image: samasemIMG,
         description: 'I created samasem.is in collaboration with my friend and colleague Arnór Daniel for a local flower wholesale. It was developed in React and uses Prismic and Commerce.js for conent management',
         link: 'https://samasem.is'
+    },
+    {
+        id:2,
+        name: 'HistoryGuessr',
+        image: HistoryGuessrBG,
+        description: 'This is an online game that I developed for fun and that I will keep developing. Players get 5 images one after another and are supposed to guess what year it was captured. I plan on adding a map to let players guess where the picture was taken aswell.',
+        link: 'https://history-guessr.vercel.app/'
     },
 ]
